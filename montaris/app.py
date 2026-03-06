0f64eb1beef0bf8fc38303c7da57fc4f6367c360
Digin Dominic <digin.dominic@ed.ac.uk>
2026-03-06 03:24:57 +0000

    Force docks expanded and toolbars hidden on launch to prevent both showing

--- montaris/app.py
+++ montaris/app.py
@@ -1274,33 +1274,18 @@ class MontarisApp(QMainWindow):
         state = self.settings.value("windowState")
         if state:
             self.restoreState(state)
-        # Ensure exactly one of docks/toolbar is visible per side
-        right_docks = [self._layer_dock, self._props_dock,
-                       self._display_dock, self._adj_dock]
-        right_docks_visible = any(d.isVisible() for d in right_docks)
-        right_toolbar_visible = self._right_toolbar.isVisible()
-        if right_docks_visible:
-            self._right_toolbar.setVisible(False)
-            self._right_collapsed = False
-        elif right_toolbar_visible:
-            self._right_collapsed = True
-        else:
-            for d in right_docks:
-                d.setVisible(True)
-            self._right_collapsed = False
+        # restoreState may make toolbars visible from saved state.
+        # Force: docks expanded, toolbars hidden on launch.
+        self._left_toolbar.setVisible(False)
+        self._left_collapsed = False
+        for d in [self._tool_dock, self._minimap_dock]:
+            d.setVisible(True)
 
-        left_docks = [self._tool_dock, self._minimap_dock]
-        left_docks_visible = any(d.isVisible() for d in left_docks)
-        left_toolbar_visible = self._left_toolbar.isVisible()
-        if left_docks_visible:
-            self._left_toolbar.setVisible(False)
-            self._left_collapsed = False
-        elif left_toolbar_visible:
-            self._left_collapsed = True
-        else:
-            for d in left_docks:
-                d.setVisible(True)
-            self._left_collapsed = False
+        self._right_toolbar.setVisible(False)
+        self._right_collapsed = False
+        for d in [self._layer_dock, self._props_dock,
+                  self._display_dock, self._adj_dock]:
+            d.setVisible(True)
 
     def closeEvent(self, event):
         self.settings.setValue("geometry", self.saveGeometry())
